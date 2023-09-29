a1644acbdb354bbddfddd2e2940ad5a83c9e9532
carina <carinasaleitao@gmail.com>
2023-09-29 20:45:54 -0300

    1st unit test

--- test/test_phonebook.py
+++ test/test_phonebook.py
@@ -1,4 +1,15 @@
+from src.phonebook import Phonebook
+
 class TestPhonebook:
 
-    def test_add(self):
-        assert False
+    def test_add_sucess(self):
+
+        # Setup
+        phonebook = Phonebook()
+        response_assert = "Numero adicionado"
+
+        # Chamada
+        response = phonebook.add("Carina", '99999999')
+
+        #Avaliação
+        assert response_assert == response
